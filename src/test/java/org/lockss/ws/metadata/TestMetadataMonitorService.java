fba99a54510367fddb7ce3f87f944e10d301fc86
Daniel Vargas <dlvargas@stanford.edu>
2022-04-12 15:01:44 -0700

    WIP: Skeleton implementation for remaining SOAP services

---
+++ src/test/java/org/lockss/ws/metadata/TestMetadataMonitorService.java
@@ -0,0 +1,88 @@
+package org.lockss.ws.metadata;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.lockss.log.L4JLogger;
+import org.lockss.spring.test.SpringLockssTestCase4;
+import org.lockss.util.rest.RestResponseErrorBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Service;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"security.basic.enabled=false"})
+public class TestMetadataMonitorService extends SpringLockssTestCase4 {
+  private static final L4JLogger log = L4JLogger.getLogger();
+
+  @TestConfiguration
+  public static class MyConfiguration {
+    @Bean
+    public RestTemplate restTemplate() {
+      return new RestTemplate();
+    }
+  }
+
+  @Autowired
+  protected Environment env;
+
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @LocalServerPort
+  private int port;
+
+  private MetadataMonitorService proxy;
+  private MockRestServiceServer mockRestServer;
+
+  private static final ObjectMapper mapper = new ObjectMapper();
+
+  private static final String TARGET_NAMESPACE = "http://metadata.ws.lockss.org/";
+  private static final String SERVICE_NAME = "MetadataMonitorServiceImplService";
+
+  private static final String USERNAME = "lockss-u";
+  private static final String PASSWORD = "lockss-p";
+  private static final String BASIC_AUTH_HASH = "Basic bG9ja3NzLXU6bG9ja3NzLXA=";
+
+  // FIXME: Blank mock REST error response
+  private static final RestResponseErrorBody.RestResponseError blankError =
+      new RestResponseErrorBody.RestResponseError();
+
+  @Before
+  public void init() throws MalformedURLException {
+    // Setup proxy to SOAP service
+    String wsdlEndpoint = "http://localhost:" + port + "/ws/MetadataMonitorService?wsdl";
+    Service srv = Service.create(new URL(wsdlEndpoint), new QName(TARGET_NAMESPACE, SERVICE_NAME));
+    proxy = srv.getPort(MetadataMonitorService.class);
+
+    // Add authentication headers for SOAP request
+    BindingProvider bp = (BindingProvider) proxy;
+    Map<String, Object> requestContext = bp.getRequestContext();
+    requestContext.put(BindingProvider.USERNAME_PROPERTY, USERNAME);
+    requestContext.put(BindingProvider.PASSWORD_PROPERTY, PASSWORD);
+
+    // Create MockRestServiceServer from RestTemplate
+    mockRestServer = MockRestServiceServer.createServer(restTemplate);
+  }
+
+  @Test
+  public void test() throws Exception {
+
+  }
+}
