cd100c7e519f10784783fb39cc541b9abf06f61c
Daniel Vargas <dlvargas@stanford.edu>
2022-05-27 00:14:54 -0700

    WIP: Implementation of DaemonStatusService tests

--- src/test/java/org/lockss/ws/status/TestDaemonStatusService.java
+++ src/test/java/org/lockss/ws/status/TestDaemonStatusService.java
@@ -37,15 +37,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.lockss.log.L4JLogger;
 import org.lockss.spring.test.SpringLockssTestCase4;
+import org.lockss.util.ListUtil;
 import org.lockss.util.rest.RestResponseErrorBody;
+import org.lockss.util.rest.RestUtil;
+import org.lockss.util.rest.status.ApiStatus;
+import org.lockss.ws.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.ExpectedCount;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
@@ -53,8 +61,13 @@ import javax.xml.namespace.QName;
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Service;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
-import java.util.Map;
+import java.util.*;
+
+import static org.lockss.ws.BaseServiceImpl.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -117,7 +130,32 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testIsDaemonReady() throws Exception {
-    // TODO
+    mockIsServiceReady(env.getProperty(REPO_SVC_URL_KEY), true);
+    mockIsServiceReady(env.getProperty(CONFIG_SVC_URL_KEY), true);
+    mockIsServiceReady(env.getProperty(POLLER_SVC_URL_KEY), true);
+    mockIsServiceReady(env.getProperty(MDX_SVC_URL_KEY), true);
+    mockIsServiceReady(env.getProperty(MDQ_SVC_URL_KEY), true);
+
+    boolean result = proxy.isDaemonReady();
+
+    assertTrue(result);
+  }
+
+  private void mockIsServiceReady(String url, boolean isReady) throws Exception {
+    // Prepare the endpoint URI
+    String statusEndpoint = url + "/status";
+    URI statusQuery = RestUtil.getRestUri(statusEndpoint, null, null);
+
+    ApiStatus apiStatus = new ApiStatus();
+    apiStatus.setReady(isReady);
+
+    // Mock REST call for ArtifactData
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(statusQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(apiStatus)));
   }
 
   /**
@@ -125,7 +163,39 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testGetAuIds() throws Exception {
-    // TODO
+    List<String> suffixes = ListUtil.list("A", "B", "C");
+    List<AuWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    for (String suffix : suffixes) {
+      AuWsResult result = new AuWsResult();
+      result.setAuId("auid" + suffix);
+      result.setName("name" + suffix);
+
+      expectedResult.add(result);
+    }
+
+    String auQuery = "select auId, name";
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("auQuery", auQuery);
+
+    // Prepare the endpoint URI
+    String auQueriesEndpoint = env.getProperty(CONFIG_SVC_URL_KEY) + "/auqueries";
+    URI auQueriesQuery = RestUtil.getRestUri(auQueriesEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(auQueriesQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    Collection<IdNamePair> result = proxy.getAuIds();
+
+    assertEquals(expectedResult.size(), result.size());
+    // TODO: Assert
   }
 
   /**
@@ -133,7 +203,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testGetAuStatus() throws Exception {
-    // TODO
+    String auId = "test";
+
+    AuStatus expectedResult = new AuStatus(); // TODO
+
+    // Prepare the URI path variables.
+    Map<String, String> uriVariables = new HashMap<>(1);
+    uriVariables.put("auId", auId);
+
+    // Prepare the endpoint URI
+    String auStatusesEndpoint = env.getProperty(CONFIG_SVC_URL_KEY) + "/austatuses/{auId}";
+    URI auStatusesQuery = RestUtil.getRestUri(auStatusesEndpoint, uriVariables, null);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(auStatusesQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    AuStatus result = proxy.getAuStatus(auId);
+
+    // TODO: Assert
   }
 
   /**
@@ -141,7 +233,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryPlugins() throws Exception {
-    // TODO
+    String pluginQuery = "plugin query";
+
+    List<PluginWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("pluginQuery", pluginQuery);
+
+    // Prepare the endpoint URI
+    String pluginsEndpoint = env.getProperty(CONFIG_SVC_URL_KEY) + "/plugins";
+    URI pluginsQuery = RestUtil.getRestUri(pluginsEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(pluginsQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<PluginWsResult> result = proxy.queryPlugins(pluginQuery);
+
+    // TODO: Assert
   }
 
   /**
@@ -149,7 +263,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryAus() throws Exception {
-    // TODO
+    String auQuery = "au query";
+
+    List<AuWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("auQuery", auQuery);
+
+    // Prepare the endpoint URI
+    String auQueriesEndpoint = env.getProperty(CONFIG_SVC_URL_KEY) + "/auqueries";
+    URI auQueriesQuery = RestUtil.getRestUri(auQueriesEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(auQueriesQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<AuWsResult> result = proxy.queryAus(auQuery);
+
+    // TODO: Assert
   }
 
   /**
@@ -157,7 +293,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryPeers() throws Exception {
-    // TODO
+    String peerQuery = "peer query";
+
+    List<PeerWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("peerQuery", peerQuery);
+
+    // Prepare the endpoint URI
+    String peersEndpoint = env.getProperty(POLLER_SVC_URL_KEY) + "/peers";
+    URI peersQuery = RestUtil.getRestUri(peersEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(peersQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<PeerWsResult> result = proxy.queryPeers(peerQuery);
+
+    // TODO: Assert
   }
 
   /**
@@ -165,7 +323,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryVotes() throws Exception {
-    // TODO
+    String voteQuery = "vote query";
+
+    List<VoteWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("voteQuery", voteQuery);
+
+    // Prepare the endpoint URI
+    String votesEndpoint = env.getProperty(POLLER_SVC_URL_KEY) + "/votes";
+    URI votesQuery = RestUtil.getRestUri(votesEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(votesQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<VoteWsResult> result = proxy.queryVotes(voteQuery);
+
+    // TODO: Assert
   }
 
   /**
@@ -173,7 +353,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryRepositorySpaces() throws Exception {
-    // TODO
+    String repositorySpaceQuery = "repository space query";
+
+    List<VoteWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("repositorySpaceQuery", repositorySpaceQuery);
+
+    // Prepare the endpoint URI
+    String repoSpacesEndpoint = env.getProperty(POLLER_SVC_URL_KEY) + "/repositoryspaces";
+    URI repoSpacesQuery = RestUtil.getRestUri(repoSpacesEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(repoSpacesQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<RepositorySpaceWsResult> result = proxy.queryRepositorySpaces(repositorySpaceQuery);
+
+    // TODO: Assert
   }
 
   /**
@@ -181,7 +383,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryRepositories() throws Exception {
-    // TODO
+    String repositoryQuery = "repository query";
+
+    List<RepositoryWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("repositoryQuery", repositoryQuery);
+
+    // Prepare the endpoint URI
+    String repositoriesEndpoint = env.getProperty(POLLER_SVC_URL_KEY) + "/repositories";
+    URI repositoriesQuery = RestUtil.getRestUri(repositoriesEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(repositoriesQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<RepositoryWsResult> result = proxy.queryRepositories(repositoryQuery);
+
+    // TODO: Assert
   }
 
   /**
@@ -189,7 +413,7 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryCrawls() throws Exception {
-    // TODO
+    // TODO: Not implemented
   }
 
   /**
@@ -197,7 +421,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryPolls() throws Exception {
-    // TODO
+    String pollQuery = "repository query";
+
+    List<PollWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("pollQuery", pollQuery);
+
+    // Prepare the endpoint URI
+    String pollsEndpoint = env.getProperty(POLLER_SVC_URL_KEY) + "/polls";
+    URI pollsQuery = RestUtil.getRestUri(pollsEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(pollsQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<PollWsResult> result = proxy.queryPolls(pollQuery);
+
+    // TODO: Assert
   }
 
   /**
@@ -205,7 +451,23 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testGetPlatformConfiguration() throws Exception {
-    // TODO
+    PlatformConfigurationWsResult expectedResult = new PlatformConfigurationWsResult(); // TODO
+
+    // Prepare the endpoint URI
+    String platformConfigEndpoint = env.getProperty(CONFIG_SVC_URL_KEY) + "/config/platform";
+    URI platformConfigQuery = RestUtil.getRestUri(platformConfigEndpoint, null, null);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(platformConfigQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    PlatformConfigurationWsResult result = proxy.getPlatformConfiguration();
+
+    // TODO: Assert
   }
 
   /**
@@ -213,7 +475,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryTdbPublishers() throws Exception {
-    // TODO
+    String tdbPublisherQuery = "tdb publisher query";
+
+    List<TdbPublisherWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("tdbPublisherQuery", tdbPublisherQuery);
+
+    // Prepare the endpoint URI
+    String tdbPublishersEndpoint = env.getProperty(CONFIG_SVC_URL_KEY) + "/tdbpublishers";
+    URI tdbPublishersQuery = RestUtil.getRestUri(tdbPublishersEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(tdbPublishersQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<TdbPublisherWsResult> result = proxy.queryTdbPublishers(tdbPublisherQuery);
+
+    // TODO: Assert
   }
 
   /**
@@ -221,7 +505,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryTdbTitles() throws Exception {
-    // TODO
+    String tdbTitleQuery = "tdb title query";
+
+    List<TdbTitleWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("tdbTitleQuery", tdbTitleQuery);
+
+    // Prepare the endpoint URI
+    String tdbTitlesEndpoint = env.getProperty(CONFIG_SVC_URL_KEY) + "/tdbtitles";
+    URI tdbTitlesQuery = RestUtil.getRestUri(tdbTitlesEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(tdbTitlesQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<TdbTitleWsResult> result = proxy.queryTdbTitles(tdbTitleQuery);
+
+    // TODO: Assert
   }
 
   /**
@@ -229,7 +535,29 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testQueryTdbAus() throws Exception {
-    // TODO
+    String tdbAuQuery = "tdb au query";
+
+    List<TdbAuWsResult> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("tdbAuQuery", tdbAuQuery);
+
+    // Prepare the endpoint URI
+    String tdbAusEndpoint = env.getProperty(CONFIG_SVC_URL_KEY) + "/tdbaus";
+    URI tdbAusQuery = RestUtil.getRestUri(tdbAusEndpoint, null, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(tdbAusQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<TdbAuWsResult> result = proxy.queryTdbAus(tdbAuQuery);
+
+    // TODO: Assert
   }
 
   /**
@@ -237,6 +565,34 @@ public class TestDaemonStatusService extends SpringLockssTestCase4 {
    */
   @Test
   public void testGetAuUrls() throws Exception {
-    // TODO
+    String auId = "auId";
+    String urlPrefix = "urlPrefix";
+
+    List<String> expectedResult = new ArrayList<>(); // TODO
+
+    // Prepare the URI path variables
+    Map<String, String> uriVariables = new HashMap<>(1);
+    uriVariables.put("collection", env.getProperty(REPO_COLLECTION_KEY));
+    uriVariables.put("auId", auId);
+
+    // Prepare the query parameters
+    Map<String, String> queryParams = new HashMap<>(1);
+    queryParams.put("urlPrefix", urlPrefix);
+
+    // Prepare the endpoint URI
+    String auArtifactsEndpoint = env.getProperty(REPO_SVC_URL_KEY) + "/collections/{collection}/aus/{auId}/artifacts";
+    URI auArtifactsQuery = RestUtil.getRestUri(auArtifactsEndpoint, uriVariables, queryParams);
+
+    mockRestServer
+        .expect(ExpectedCount.once(), requestTo(auArtifactsQuery))
+        .andExpect(method(HttpMethod.GET))
+        .andExpect(header("Authorization", BASIC_AUTH_HASH))
+        .andRespond(withStatus(HttpStatus.OK)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(mapper.writeValueAsString(expectedResult)));
+
+    List<String> result = proxy.getAuUrls(auId, urlPrefix);
+
+    // TODO: Assert
   }
 }
